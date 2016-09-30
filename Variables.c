ee5c733268a1e48aa319e6d3d44b3c84d1706c70
Dubascudes <wenglish0223@gmail.com>
2016-09-30 13:49:21 -0400

    This thing is riddled with bugs. Find them and correct them.
    Pro-tip: Coding in your IDE and attempting a compile will give you error warnings if there are any non-logical errors.

---
+++ Variables.c
@@ -0,0 +1,21 @@
+void varOne;  #declare variable one
+void varTwo; #declare variable two
+void varThree; #declare variable three
+int main{}; #begin main function
+  void varOne:  
+  void varOne:
+  void varTwo:
+  void varOne:
+  void varTwo:
+} 
+  varOne[ #define varOne
+  move(20,2) #move motor 2 at 20% power
+  move(20,1) #move motor 1 at 20% power
+  sleep(5000) #for 5 seconds
+ ]
+ varTwo[  #define varTwo
+  move(20,2)
+  move(00,1)
+  sleep(5)
+ 
+   return()
